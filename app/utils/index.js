5edf3b45c2413ca584ced0e01ec166c11d893477
Igor <isobayev@gmail.com>
2023-09-03 20:19:28 +0200

    Validation utils

---
+++ app/utils/index.js
@@ -0,0 +1,31 @@
+import Validator from 'better-validator'
+import VError from 'verror'
+
+/**
+ * Validates a request with the specified validation schema
+ * @param {Request} request
+ * @param {ValidateRequestCallback} validations function that expose to you the request object for your validations
+ */
+function validateRequest (request, validations) {
+    let allowedFields = ['params', 'query', 'body', 'file']
+    if (!request.file) request.file = undefined
+    const validator = new Validator()
+    validator(request).required().isObject(req => {
+      let properties = _(request)
+        .pick(allowedFields)
+        .reduce((accum, value, key) => {
+          let propValidator
+          req(key).isObject(validator => { propValidator = validator })
+          accum[key] = propValidator
+          return accum
+        }, {})
+      validations(properties)
+    })
+    let errors = validator.run()
+    if (validator.run().length) return new VError.WError({name: 'FieldsValidationError', info: errors}, 'Fail validating fields')
+}
+
+
+export default {
+    validateRequest,
+}
