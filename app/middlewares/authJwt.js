4c61ccd994a4f7f742bc8a7137b319280b8123b1
Igor <isobayev@gmail.com>
2023-09-06 22:33:38 +0200

    New middleware function to validate auth user and user request info

--- app/middlewares/authJwt.js
+++ app/middlewares/authJwt.js
@@ -21,6 +21,32 @@ export const verifyToken = (req, res, next) => {
   })
 }
 
+export const verifyUserAndToken = (req, res, next) => {
+  let token = req.session.token
+
+  if (!token) {
+    return res.status(403).send({ message: 'No token provided!' })
+  }
+
+  jwt.verify(token, auth.secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({
+        message: 'Unauthorized!',
+      })
+    }
+    req.userId = decoded.id
+
+    if (req.userId !== req.params.id) {
+      return res.status(401).send({
+        message: 'Unauthorized!',
+      })
+    }
+
+    next()
+  })
+}
+
+
 export const isAdmin = async (req, res, next) => {
   try {
     const user = await User.findById(req.userId)
@@ -40,6 +66,7 @@ export const isAdmin = async (req, res, next) => {
 const authJwt = {
   verifyToken,
   isAdmin,
+  verifyUserAndToken,
 }
 
 export default authJwt
