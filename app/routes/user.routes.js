4cd5a46c94a19f6f843f71fd8f369d5993cd5fc7
Igor <isobayev@gmail.com>
2023-09-10 16:40:49 +0200

    Implement file management

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -3,13 +3,19 @@ import allAccess from '../controllers/users/users.allAccess.controller.js'
 import details from '../controllers/users/users.details.controller.js'
 import adminBoard from '../controllers/users/users.details.controller.js'
 import edit from '../controllers/users/users.edit.controller.js'
+import uploadFile from '../controllers/users/users.uploadFile.controller.js'
 
 import express from 'express'
+import multer from 'multer'
+
 const router = express.Router()
+
+const uploader = multer({ dest: '/tmp' })
 
 router.get('/:id', [authJwt.verifyUserAndToken], details)
 
 router.put('/edit/:id', [authJwt.verifyUserAndToken], edit)
+router.post('/upload-file/:id', [authJwt.verifyUserAndToken], uploader.single('file'), uploadFile)
 
 router.get('/all', allAccess)
 
