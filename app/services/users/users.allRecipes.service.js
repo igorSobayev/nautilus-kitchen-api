444db5bf13153bd937fa28bd63e6c150543e53b4
Igor <isobayev@gmail.com>
2023-10-28 13:26:45 +0200

    Update services names

--- app/services/users/users.allRecipes.service.js
+++ app/services/users/users.allRecipes.service.js
@@ -1,6 +1,6 @@
 import RecipeModel from '../../repository/recipes/recipe.model.js'
 
-export default async function details ({ userId }) {
+export default async function allRecipes ({ userId }) {
     const recipes = await RecipeModel.find({ user: userId }).lean()
 
     return recipes
--- app/services/users/users.publishedRecipes.service.js
+++ app/services/users/users.publishedRecipes.service.js
@@ -1,6 +1,6 @@
 import RecipeModel from '../../repository/recipes/recipe.model.js'
 
-export default async function details ({ userId }) {
+export default async function publishedRecipes ({ userId }) {
     const recipes = await RecipeModel.find({ user: userId, published: true, deleted: false }).lean()
 
     return recipes
