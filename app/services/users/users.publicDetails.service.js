c8dd43bf189c87b27ca16aec0a4ed9c845d97b48
Igor <isobayev@gmail.com>
2023-11-09 12:47:22 +0100

    Field validation

--- app/services/users/users.publicDetails.service.js
+++ app/services/users/users.publicDetails.service.js
@@ -2,6 +2,11 @@ import UserModel from '../../repository/users/user.model.js'
 import VError from 'verror'
 
 export default async function publicDetails ({ username }) {
+
+    if (!username) {
+        throw VError(`Missing username`)
+    }
+
     const rawUser = await UserModel.findOne({ username }).lean()
 
     if (!rawUser) {
